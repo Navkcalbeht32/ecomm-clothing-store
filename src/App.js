a0fe9721305ab8dc06fa59ec28c5717107deee3c
Navkcalbeht32 <asumoski@gmail.com>
2020-12-26 18:38:51 -0500

    Header build, generated shop components page, added styles, preview page logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
 
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './shop/shop.component';
+import Header from './components/header/header.component';
+
+
+
+const HatsPage = () => (
+  <div>
+    Hats Page
+  </div>
+)
 function App() {
   return (
-    <div>
-      <HomePage />
+    <div className="body">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
